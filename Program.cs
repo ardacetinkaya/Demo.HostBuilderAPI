6bbf05cf14cee0db7d967e3dd6bc61d0e441b089
Arda Cetinkaya <Arda@Ardas-MacBook-Pro.local>
2018-03-18 20:08:05 +0300

    Logging

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace SomeKindOfProcessor
 {
@@ -12,7 +13,7 @@ namespace SomeKindOfProcessor
         static async Task Main(string[] args)
         {
             var hostBuilder = new HostBuilder()
-            .ConfigureHostConfiguration(config=>
+            .ConfigureHostConfiguration(config =>
             {
                 config.AddEnvironmentVariables();
             })
@@ -21,12 +22,16 @@ namespace SomeKindOfProcessor
                 config.SetBasePath(Directory.GetCurrentDirectory());
                 config.AddJsonFile("appsettings.json", optional: true);
                 config.Build();
+            }).ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
             })
             .ConfigureServices((hostContext, services) =>
             {
                 services.AddSingleton<ICustomProcessorData>(serviceProvider =>
                 {
-                    
+
                     return new CustomProcessorData("Dummy Processor");
                 });
                 services.AddScoped<IHostedService, CustomProcessor>();
